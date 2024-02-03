f1f1ebeb7e7ec2f0c14c8eead7a5ec05228b2f7b
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-02-03 13:59:02 -0800

    added other colors

--- Components/Color.js
+++ Components/Color.js
@@ -1,10 +1,13 @@
 const color = {
     background: '#AAA8C8',
+    cardBackground: '#363679',
     text: '#3B387E',
     message: '#3B3654',
+    gray: '#838387',
     warning: '#9A1464',
     transparent: 'rgba(0,0,0,0)',
     invalid: 'white',
+    alert: 'FDB80E',
 }
 
 export default color;
