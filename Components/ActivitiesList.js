cf840bf5ea752bf9ca8574ba7c2fa004a49a44f7
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-02-07 21:03:50 -0800

    changed to flatList for the activity list

--- Components/ActivitiesList.js
+++ Components/ActivitiesList.js
@@ -1,45 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import React, { useContext } from 'react';
 import { ActivityContext } from './ActivityContext';
 import { color, spacing } from './StyleHelper';
 
+const ActivityItem = ({ activity }) => {
+  return (
+    <View style={styles.activity}>
+      <Text style={styles.name}>
+        {activity.name}
+      </Text>
+      {activity.special === true && (
+        <View style={styles.iconContainer}>
+          <Text style={styles.icon}>!</Text>
+        </View>
+      )}
+      <Text style={styles.date}>{activity.date}</Text>
+      <Text style={styles.duration}>{activity.duration} min</Text>
+    </View>
+  );
+};
+
 export default ActivitiesList = ({ activityType }) => {
-  
   const { activities } = useContext(ActivityContext);
 
   const filteredActivities = activities.filter(activity => 
     activityType === 'all' || (activityType === 'special' && activity.special)
   );
 
-  console.log(filteredActivities);
-  
+  const renderItem = ({ item }) => <ActivityItem activity={item} />;
+
   return (
-    <View style={styles.container}>
-      {filteredActivities.map((activity, index) => (
-        <View key={index} style={styles.activity}>
-          <Text style={styles.name}>
-            {activity.name}
-            {activity.special}
-          </Text>
-          {activity.special === true && (
-            <View style={styles.iconContainer}>
-              <Text style={styles.icon}>!</Text>
-            </View>
-          )}
-          <Text style={styles.date}>{activity.date}</Text>
-          <Text style={styles.duration}>{activity.duration} min</Text>
-        </View>
-      ))}
-    </View>
+    <FlatList
+      style={styles.container}
+      data={filteredActivities}
+      renderItem={renderItem}
+      keyExtractor={(item, index) => index.toString()}
+      contentContainerStyle={styles.container}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '85%',
+    width: '100%',
+    // borderWidth: 1,
     backgroundColor: color.transparent,
-    paddingTop: spacing.xlarge,
+    paddingTop: spacing.large,
+    paddingHorizontal: spacing.medium,
   },
   activity: {
     flexDirection: 'row',
