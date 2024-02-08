2b495ee665d170ae01f299fbb0e657aaa31b9471
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-02-07 22:10:52 -0800

    Added comments on DropDownList.js

--- Components/DropDownList.js
+++ Components/DropDownList.js
@@ -3,6 +3,15 @@ import { StyleSheet } from 'react-native';
 import DropDownPicker from "react-native-dropdown-picker";
 import { color } from './StyleHelper';
 
+/**
+ * Render the DropDownList component with placeholder, listItems, and handleItemSelect properties.
+ * 
+ * @param {object} props - DropDownList properties
+ * @param {string} props.placeholder - dropdown placeholder
+ * @param {array} props.listItems - dropdown list items
+ * @param {function} props.handleItemSelect - dropdown item select handler
+ * @returns {JSX.Element} - DropDownList component
+ */
 export default DropDownList = ({ placeholder, listItems, handleItemSelect }) => {
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState(null);
@@ -16,6 +25,12 @@ export default DropDownList = ({ placeholder, listItems, handleItemSelect }) => 
         }
     }, []);
 
+    /**
+     * Handle the selected item from the dropdown list
+     * 
+     * @param {string} itemValue - selected item value
+     * @returns {void}
+     */
     const handleValueChange = (itemValue) => {
         setValue(itemValue);
         handleItemSelect(itemValue);
