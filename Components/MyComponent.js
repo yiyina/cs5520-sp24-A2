0f808d96de58e8731b5a0dc506b21aaaccbbe8c1
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-02-07 22:12:39 -0800

    Added comments on MyComponent.js

--- Components/MyComponent.js
+++ Components/MyComponent.js
@@ -2,9 +2,22 @@ import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
+/**
+ * Render the MyComponent component.
+ * 
+ * @param {object} props - MyComponent properties
+ * @returns {JSX.Element} - MyComponent component
+ */
 const MyComponent = () => {
   const [date, setDate] = useState(new Date());
 
+  /**
+   * Handle the date change event.
+   * 
+   * @param {Event} event - date change event
+   * @param {Date} selectedDate - selected date
+   * @returns {void}
+   */
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
     setDate(currentDate);
