49e9a0f202ceea821025468986531abec901e123
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-02-07 22:06:03 -0800

    Added comments on Button.js

--- Components/Button.js
+++ Components/Button.js
@@ -2,6 +2,14 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { spacing } from './StyleHelper';
 
+/**
+ * Render the Button component with text and color properties 
+ * and a click handler function to handle button click events 
+ * and a disabled property to disable the button if needed.
+ * 
+ * @param {object} props - button properties
+ * @returns {JSX.Element} - Button component
+ */
 export default Button = ({ text, textColor, handleClick, disabled, style }) => {
   const defaultStyles = StyleSheet.create({
     textStyle: {
