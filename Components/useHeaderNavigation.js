bfeb9be72bbec8823ec482ed590856fecf6e0311
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-02-07 22:15:22 -0800

    Added comments on useHeaderNavigation.js

--- Components/useHeaderNavigation.js
+++ Components/useHeaderNavigation.js
@@ -2,7 +2,16 @@ import { useLayoutEffect } from 'react';
 import Button from './Button';
 import { color, spacing, fontSize } from './StyleHelper';
 
+/**
+ * Use header navigation based on the type of navigation.
+ * 
+ * @param {object} navigation - navigation object
+ * @param {string} navigation.setOptions - set navigation options
+ * @param {string} type - navigation type
+ * @returns {void}
+ */
 const useHeaderNavigation = (navigation, type) => {
+  // Set header navigation options based on the type of navigation.
   useLayoutEffect(() => {
     if (type == 'Add') {
       navigation.setOptions({
