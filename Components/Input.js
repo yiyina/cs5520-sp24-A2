4eef6b68ff89a68c2fe07ee7b9564c017e65733a
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-02-07 22:11:44 -0800

    Added comments on Input.js

--- Components/Input.js
+++ Components/Input.js
@@ -2,8 +2,22 @@ import { StyleSheet, View, TextInput } from 'react-native';
 import React from 'react';
 import { color, spacing } from './StyleHelper';
 
+/**
+ * Render the Input component with text, handleInput and onFocus properties.
+ * 
+ * @param {object} props - input properties
+ * @param {string} props.text - input text
+ * @param {function} props.handleInput - input text change event handler
+ * @param {function} props.onFocus - input focus event handler
+ * @returns {JSX.Element} - Input component
+ */
 export default Input = ({ text, handleInput, onFocus }) => {
 
+  /**
+   * Handle text change event and call handleInput function.
+   * 
+   * @param {string} text - input text
+   */
   const textChange = (text) => {
     handleInput(text);
   }
