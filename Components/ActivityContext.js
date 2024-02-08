77d3505801c197e5833d94008dceba64d2fc5661
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-02-07 22:04:34 -0800

    Added comments on ActivityContext.js

--- Components/ActivityContext.js
+++ Components/ActivityContext.js
@@ -1,7 +1,14 @@
 import React, { createContext, useState } from 'react';
 
+// Create a context for the activity data
 export const ActivityContext = createContext();
 
+/**
+ * Render the ActivityProvider component.
+ *  
+ * @param {object} children - child components
+ * @returns {JSX.Element} - ActivityProvider component
+ */
 export const ActivityProvider = ({ children }) => {
     const [activities, setActivities] = useState([]);
 
